12fb2fa6af30b0043a667fad140109f223c41e69
shuhei.takai <stst.shu82@gmail.com>
2019-08-04 16:48:13 +0000

    user sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
+  get 'users/edit'
+  get 'users/update'
   get 'toppages/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   root to: 'toppages#index'
+  
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create, :edit, :update]
 
 
 
